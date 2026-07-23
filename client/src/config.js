e0cd40b020f15995167603ad476504d0b5883536
Pomodor12 <167606195+Pomodor12@users.noreply.github.com>
2026-07-23 15:28:27 +0700

    Fix API_URL for dev/production environments

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,5 @@
-const API_URL = process.env.REACT_APP_API_URL || window.location.origin;
+const API_URL = process.env.NODE_ENV === 'production'
+  ? window.location.origin
+  : 'http://localhost:3001';
 
 export { API_URL };
